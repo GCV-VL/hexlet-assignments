d49bb00bb3fb6870129017a9ff37e19089674b60
Vladimir <gcvqa@mail.ru>
2023-03-13 21:26:05 +1000

    autosave

--- rails-basics-ru/static-pages/config/routes.rb
+++ rails-basics-ru/static-pages/config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   # BEGIN
-  
+  root 'HomeController#index'
+  get 'pages/index'
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
