ecf314f42e191e64ffea48f1152675899a6322a0
@hexlet/cli <support@hexlet.io>
2023-03-15 19:53:09 +1000

    submit ruby-basics-ru/metaprogramming

--- ruby-basics-ru/metaprogramming/lib/model.rb
+++ ruby-basics-ru/metaprogramming/lib/model.rb
@@ -1,40 +1,61 @@
-require 'date'
+# frozen_string_literal: true
 
+# BEGIN
 module Model
-    def self.included(base)
-      base.extend ClassMethods
-      base.attr_reader :attr
+  def initialize(attrs = {})
+    @attributes = {}
+    self.class.options.each do |name, options|
+      value = attrs[name] || options[:default]
+      set value, to: name
+    end
+  end
+
+  module ClassMethods
+    def options
+      @options || {}
     end
 
-    module ClassMethods
-      def attribute(name, options = {})
-        @attributes ||= {}
-        attributes[name] = options
+    def converting(input, to:)
+      return input if input.nil?
 
-        define_method("#{name}=") do
-            @attr[name]
-        end
-
-        define_method("#{name}=") do |value|
-            write_attr(name, value)
-        end
-      end
-
-      def attributes
-        @attributes ||= {}
+      case to
+      when :integer
+        input.to_i
+      when :string
+        input.to_s
+      when :boolean
+        input == true
+      when :datetime
+        DateTime.parse(input)
+      else
+        raise 'Unknown type!'
       end
     end
 
-    def initialize(attributes = {})
-      attributes.each do |name, value|
-        next unless self.class.attributes.key?(name)
-        send("#{name}=", value)
-      end
-    end
+    def attribute(name, **options)
+      @options ||= {}
+      @options[name] = options
 
-    def attributes
-      self.class.attributes.keys.each_with_object({}) do |name, result|
-        result[name] = send(name)
+      define_method name do
+        @attributes[name]
+      end
+
+      define_method "#{name}=" do |value|
+        set value, to: name
       end
     end
   end
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def set(value, to:)
+    options = self.class.options[to]
+    @attributes[to] = self.class.converting value, to: options[:type]
+  end
+
+  def attributes
+    @attributes
+  end
+end
