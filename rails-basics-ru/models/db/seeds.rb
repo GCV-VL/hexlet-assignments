eb0b09435db8aae309f5723622e575639a91e541
@hexlet/cli <support@hexlet.io>
2023-03-17 23:02:19 +1000

    submit rails-basics-ru/models

--- rails-basics-ru/models/db/seeds.rb
+++ rails-basics-ru/models/db/seeds.rb
@@ -6,3 +6,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+ 10.times do
+      Article.create(
+        title: Faker::Book.title,
+        body: Faker::Quote.famous_last_words
+      )
+    end
