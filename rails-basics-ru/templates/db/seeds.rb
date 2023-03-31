c6377a7153573fbd592f3d054c95719b976d1539
Vladimir <gcvqa@mail.ru>
2023-04-01 09:27:05 +1000

    relocate

--- rails-basics-ru/templates/db/seeds.rb
+++ rails-basics-ru/templates/db/seeds.rb
@@ -1,6 +1,29 @@
+# frozen_string_literal: true
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  completed   :boolean
+#  creator     :string
+#  description :text
+#  name        :string
+#  performer   :string
+#  status      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'faker'
-40.times do |index|
-    Task.create(
+100.times do |index|
+    Task.create!(
         creator: Faker::Name.unique.name,
         completed: false,
         description: Faker::Lorem.paragraph,
